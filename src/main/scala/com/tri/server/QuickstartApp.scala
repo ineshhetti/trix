cb0e5651dd5bdf0485cd6cb66acddc34ae6319a7
Inesh <ineshhetti@gmail.com>
2021-01-30 14:54:24 +0700

    port

--- src/main/scala/com/tri/server/QuickstartApp.scala
+++ src/main/scala/com/tri/server/QuickstartApp.scala
@@ -16,7 +16,7 @@ object QuickstartApp {
     // Akka HTTP still needs a classic ActorSystem to start
     import system.executionContext
     //Http().bindAndHandle(MainRouter.routes, config.getString("http.interface"), config.getInt("http.port"))
-    val futureBinding = Http().newServerAt("localhost", 8080).bind(routes)
+    val futureBinding = Http().newServerAt("0.0.0.0", 8080).bind(routes)
     futureBinding.onComplete {
       case Success(binding) =>
         val address = binding.localAddress
